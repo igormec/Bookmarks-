ce246d4840b23920ebc044fd06ee1c67686867c5
Igor Mec <igor_mec@hotmail.com>
2017-05-20 02:22:14 -0400

    Bookmarks now show up as a list in the browser's bookmark manager

--- bmplus.js
+++ bmplus.js
@@ -1,22 +1,38 @@
 
 
+//Shows all bookmarks as a list of links
 function showAll(){
-	var allNodes = chrome.bookmarks.getTree(function(){
-		$("#bm-list").append(allNodes);
-		//console.log(allNodes);
+	var allNodes = chrome.bookmarks.getTree(function(allNodes){
+		$("#bm-list").append(parseNodes(allNodes));
 	});
-
-	/*var list = $('<ul>');
-
-	for(var i = 0; i < allNodes.length; i++){
-		list.append()
-	}*/
 }
 
+//Goes through the list returned by chrome.bookmarks.getTree()
+function parseNodes(nodeList){
+	var list = $("<ul>");
+	for(var i = 0;i < nodeList.length;i++){
+		list.append(showNode(nodeList[i]));
+	}
+	return list;
+}
 
-/*function getAllNodes(){}*/
-
-
+//Exxtracts the data from each individual bookmarkNode
+function showNode(node){
+	if(node.title){
+		var anc = $("<a>");
+		anc.attr("href", node.url);
+		anc.text(node.title);
+		var span = $("<span>");
+		span.append(anc);
+	}
+	var li = $(node.title ? "<li>" : "<div>").append(span);
+	
+	//For folders, recurse into parseNodes to read the childrenNodeTree
+	if (node.children && node.children.length > 0){
+		li.append(parseNodes(node.children));
+	}
+	return li;
+}
 
 document.addEventListener('DOMContentLoaded', function () {
   showAll();
