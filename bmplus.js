a79a5bf04fe5cef7faa5979b9b0ad350f5b46513
Igor M <igor_mec@hotmail.com>
2017-05-23 05:44:30 -0400

    Start animation for bookmarks browser table

--- bmplus.js
+++ bmplus.js
@@ -1,5 +1,7 @@
 
 
+
+
 //Shows all bookmarks as a list of links
 function showAll(){
 	var allNodes = chrome.bookmarks.getTree(function(allNodes){
@@ -9,28 +11,32 @@ function showAll(){
 
 //Goes through the list returned by chrome.bookmarks.getTree()
 function parseNodes(nodeList){
-	var list = $("<ul>");
-	for(var i = 0;i < nodeList.length;i++){
-		list.append(showNode(nodeList[i]));
+	//var list = $("<ul>");
+
+	/*for(var i = 0;i < Math.ceil(nodeList.length/4);i++){
+
+		var row = $("<tr>");
+		for(var j = 0; j < 4;j++){
+			row.append(showNode(nodeList[i]));
 	}
-	return list;
+	return list;*/
 }
 
 //Extracts the data from each individual bookmarkNode
 function showNode(node){
 	if(node.title){
 		/*var anc = $("<a>");
-		anc.attr("href", node.url);
+		anc.attr("href", node.url
 		anc.text(node.title);
 		var span = $("<span>");
 		span.append(anc).append(" - " +new Date(node.dateAdded));*/
 
-		var row = $("<tr>");
-		row.text(node.title);
-
+		var span = $("<span>");
+		var td = $("<td>");
+		td.text(node.title);
 
 	}
-	var li = $(node.title ? "<li>" : "<div>").append(span);
+	//var li = $(node.title ? "<li>" : "<div>").append(span);
 	
 	//For folders, recurse into parseNodes to read the childrenNodeTree
 	if (node.children && node.children.length > 0){
@@ -40,5 +46,18 @@ function showNode(node){
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  showAll();
-});
+  	showAll();
+
+  	$(".mainListItem").hover(
+		function() {
+			
+			//$(this).animate({"opacity":0}, 1000);
+			$(this).animate({"backgroundColor":"#252525"}, 100);
+		},
+		function() {
+			$(this).animate({"backgroundColor":"#494949"}, 100);
+		}
+	);
+	
+
+})
